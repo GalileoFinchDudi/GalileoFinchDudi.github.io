fe16807c61d7854fa0d3e20cd0483d4167ba2048
Galileo Finch <galileofinchdudi@gmail.com>
2023-06-16 18:23:48 +0800

    更新内容+action自动部署

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -43,6 +43,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           },
         ],
       },
+      {
+        text: '全沾工程师',
+        link: '/fullstack/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+      },
       // {
       //   text: '页面',
       //   link: '/ui/',
